8457a728f84fb9cf3982169ffed27cb00d0dfa31
HYPER097 <hyperop615@gmail.com>
2024-11-22 15:01:47 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -28,7 +28,7 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 
-# Database to save your chats and stats... Get MongoDB:-  https://telegra.ph/How-To-get-Mongodb-URI-04-06
+# Database to save your chats and stats... Get MongoDB:-  
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 
